dbd1c5b33e73e4940bacb6f7be59407ddf6cbbe6
Guido <Asbun>
2020-02-04 13:37:37 -0800

    stockInfo function

--- Assets/guidoasbun.js
+++ Assets/guidoasbun.js
@@ -1,13 +1,17 @@
+function stockInfo(stockSymbol, currentDate) {
+  let stockData
+  $.getJSON(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=ups&interval=5min&apikey=MF50LI0Q6H9V0VWV`, function (data) {
 
+    stockData = {
+      open: data['Time Series (Daily)']['2020-02-04']['1. open'],
+      high: data['Time Series (Daily)']['2020-02-04']['2. high'],
+      low: data['Time Series (Daily)']['2020-02-04']['3. low'],
+      close: data['Time Series (Daily)']['2020-02-04']['4. close']
+    }
+    console.log(stockData)
 
-  function stockInfo(stockSymbol) {
-    
-    fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=UPS&interval=5min&apikey=MF50LI0Q6H9V0VWV')
-          .then(r => r.json())
-          .then(r =>
-            console.log(r['Time Series (Daily)']['2020-02-04']['1. open']))
-         .catch(e => console.log(e))
-   }
-  stockInfo(stockSymbol)
+  })
 
+    .catch(e => console.log(e))
+}
   
