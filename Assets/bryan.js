05f20ca519173be03191e7676ca03cd6689d35e6
Bryan Y <xpreyx@gmail.com>
2020-02-05 11:23:36 -0800

    watchlist backend

---
+++ Assets/bryan.js
@@ -0,0 +1,38 @@
+// RETRIEVE DATA FROM LOCALSTORAGE
+let ciData = JSON.parse(localStorage.getItem('crypit')) || []
+
+// CIDATA STRUCTURE, EXAMPLE VALUES
+ciData = {
+  crypto: 'BTC',
+  stocks: ['FB', 'AMZN', 'AAPL', 'NFLX', 'GOOG']
+}
+
+// GENERATE WATCHLIST
+document.getElementById('').innerHTML = ''
+ciData.stocks.forEach(element => {
+  document.getElementById('').innerHTML += ''
+})
+
+// RETURNS TRUE IF ALREADY IN WATCHLIST, FALSE IF NOT
+// X = STOCK SYMBOL
+const isWatched = x => {
+  return ciData.stocks.includes(x)
+}
+
+// ADDS TO WATCHLIST, UPDATE WATCHLIST DISPLAY AFTER CALLING
+// X = STOCK SYMBOL
+const addWatch = x => {
+  if (!ciData.stocks.includes(x)) ciData.stocks.unshift(x)
+}
+
+// REMOVES FROM WATCHLIST, UPDATE WATCHLIST DISPLAY AFTER CALLING
+// X = STOCK SYMBOL
+const delWatch = x => {
+  const index = ciData.stocks.indexOf(x)
+  if (index >= 0) {
+    ciData.stocks.splice(index, 1)
+  }
+}
+
+// STORE DATA TO LOCALSTORAGE
+localStorage.setItem('crypit', JSON.stringify(ciData))
