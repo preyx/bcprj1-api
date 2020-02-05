30c57ceb8e2db529128263f6ecb06bdf56a193cd
Guido <Asbun>
2020-02-05 10:50:02 -0800

    Moved code to script.js

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -2,6 +2,38 @@ $(document).ready(_ => {
   $('select').formSelect()
 })
 
+$("#makeApiCall").on('click', function () {
+  let stockSymbol = $("#searchBox").val()
+  let currentTime = moment()
+  let currentDate = currentTime.format('YYYY-MM-DD')
+  console.log(currentDate)
+  console.log(stockSymbol)
+
+  let stockData = {}
+  $.getJSON(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stockSymbol}&interval=5min&apikey=MF50LI0Q6H9V0VWV`, function (data) {
+    stockData = {
+      open: data['Time Series (Daily)'][currentDate]['1. open'],
+      high: data['Time Series (Daily)'][currentDate]['2. high'],
+      low: data['Time Series (Daily)'][currentDate]['3. low'],
+      close: data['Time Series (Daily)'][currentDate]['4. close']
+    }
+
+    
+
+
+
+
+
+
+    console.log(stockData)
+  })
+    .catch(e => console.log(e))
+  // }
+  // stockInfo(stockSymbol, currentDate)
+})
+
+
+
 // let movie = {}
 // document.getElementById('searchMovie').addEventListener('click', event => {
 //   event.preventDefault()
